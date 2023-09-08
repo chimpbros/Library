b13495c2a3dd79acf618b581fa22b46a4e51666e
chimpbros <kevinnue00@gmail.com>
2023-09-08 21:47:34 +0700

    create foreach loop to populate the table from the array

--- book.js
+++ book.js
@@ -20,3 +20,27 @@ function addBookToLibrary(title, author, pages, read) {
 
 addBookToLibrary('Hyperion cantos', 'Dan simmon', 563, 'read');
 addBookToLibrary('12 Rules for Life', 'Jordan Peterson', 763, 'read');
+
+console.log(myLibrary);
+
+const table = document.querySelector('.table tbody');
+
+// iterate over the array and populate table
+myLibrary.forEach((book) => {
+    const row = document.createElement('tr');
+    const titleCol = document.createElement('td');
+    const authorCol = document.createElement('td');
+    const pagesCol = document.createElement('td');
+    const readCol = document.createElement('td');
+
+    titleCol.textContent = book.title;
+    authorCol.textContent = book.author;
+    pagesCol.textContent = book.pages;
+    readCol.textContent = book.read;
+
+    row.appendChild(titleCol);
+    row.appendChild(authorCol);
+    row.appendChild(pagesCol);
+    row.appendChild(readCol);
+    table.appendChild(row);
+});
